1231533e91d894adfe93f037a472730b7af9f1b1
Tuan Vo <tuan.vo@sotatek.com>
2020-11-03 11:03:53 +0700

    add react hook

--- sample/src/components/Navbar.js
+++ sample/src/components/Navbar.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import { Link, NavLink, withRouter } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link, NavLink, withRouter } from "react-router-dom";
 
 const Navbar = (props) => {
+  const [name, setName] = useState("Poke Time");
+
   return (
     <nav className="nav-wrapper red darken-3">
       <div className="container">
-        <Link className="brand-logo" to="/">Poke' Times</Link>
+        <Link className="brand-logo" to="/">
+          {name}
+        </Link>
+        {/* <button onClick={() => setName("Posts")}>Posts</button>
+        <button onClick={() => setName("Users")}>Users</button>
+        <button onClick={() => setName("Comments")}>Comments</button> */}
         <ul className="right">
-          <li><NavLink exact to="/">Home</NavLink></li>
-          <li><NavLink to='/about'>About</NavLink></li>
-          <li><NavLink to='/contact'>Contact</NavLink></li>
+          <li>
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
         </ul>
       </div>
-    </nav> 
-  )
-}
+    </nav>
+  );
+};
 
-export default withRouter(Navbar)
+export default withRouter(Navbar);
